d86b6a8ac3dcfe2cbabf1404515f9e01c9f6df85
michalyao <yaoyao0777@gmail.com>
2016-12-08 21:14:06 +0800

    class example.

---
+++ src/Point.scala
@@ -0,0 +1,9 @@
+class Point(var x: Int, var y: Int) {
+
+  def move(dx: Int, dy: Int): Unit = {
+    x = x + dx;
+    y = y + dy;
+  }
+  override def toString: String =
+    "(" + x + ", " + y + ")"
+}
